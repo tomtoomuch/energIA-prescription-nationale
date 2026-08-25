11ad4c2689061895f8f8ecdff6a6b4c6a2041eb9
Thomas DUPRESSOIR <thomas.dupressoir@gmail.com>
2026-08-25 09:55:45 +0200

    <feat>routing depuis la gateway vers le MS2

--- gateway/index.js
+++ gateway/index.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require("path");
 const port = process.env.GATEWAY_PORT || 3000;
 const PYTHON_API_URL = process.env.PYTHON_SERVICE_URL || "http://ms-python-1:8000";
+const PYTHON_API_URL_2 = process.env.PYTHON_SERVICE_URL_2 || "http://ms-python-2:8002";
 const SECURITY_TOKEN = process.env.SECURITY_TOKEN;
 
 app.use(express.json());
@@ -29,6 +30,15 @@ app.get("/health", (req, res) => {
 app.get("/health-ms", async (req, res) => {
     try {
         const response = await axios.get(`${PYTHON_API_URL}/health`);
+        return res.status(200).json({ success: true, response: response.data });
+    } catch (error) {
+        return handlePythonError(error, res);
+    }
+});
+
+app.get("/health-ms-2", async (req, res) => {
+    try {
+        const response = await axios.get(`${PYTHON_API_URL_2}/health`);
         return res.status(200).json({ success: true, response: response.data });
     } catch (error) {
         return handlePythonError(error, res);
@@ -69,6 +79,36 @@ app.post("/simulate", async (req, res) => {
     try {
         console.log(`[POST] /simulate -> ${PYTHON_API_URL}/simulate`, req.body);
         const response = await axios.post(`${PYTHON_API_URL}/simulate`, req.body, { headers: pythonHeaders() });
+        return res.status(200).json({ success: true, response: response.data });
+    } catch (error) {
+        return handlePythonError(error, res);
+    }
+});
+
+app.get("/phase1/plants", async (req, res) => {
+    try {
+        console.log(`[GET] /plants -> ${PYTHON_API_URL_2}/phase1/plants`);
+        const response = await axios.get(`${PYTHON_API_URL_2}/phase1/plants`, { headers: pythonHeaders() });
+        return res.status(200).json({ success: true, response: response.data });
+    } catch (error) {
+        return handlePythonError(error, res);
+    }
+});
+
+app.get("/phase1/consumption", async (req, res) => {
+    try {
+        console.log(`[GET] /regions -> ${PYTHON_API_URL_2}/phase1/consumption`);
+        const response = await axios.get(`${PYTHON_API_URL_2}/phase1/consumption`, { headers: pythonHeaders() });
+        return res.status(200).json({ success: true, response: response.data });
+    } catch (error) {
+        return handlePythonError(error, res);
+    }
+});
+
+app.get("/phase1/simulate-day", async (req, res) => {
+    try {
+        console.log(`[GET] /regions -> ${PYTHON_API_URL_2}/phase1/simulate-day`);
+        const response = await axios.get(`${PYTHON_API_URL_2}/phase1/simulate-day`, { headers: pythonHeaders() });
         return res.status(200).json({ success: true, response: response.data });
     } catch (error) {
         return handlePythonError(error, res);
