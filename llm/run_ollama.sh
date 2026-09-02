bfeac8b9268cc61be4521d7b0258d9e453fb7969
Thomas DUPRESSOIR <thomas.dupressoir@gmail.com>
2026-09-02 11:48:25 +0200

    <feat>Conteneur ollama

--- llm/run_ollama.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Starting Ollama Server..."
-ollama serve & # Start Ollama in the background
-
-echo "Ollama is ready, creating the model..."
-
-ollama create gemma_model -f llm/Modelfile
-ollama run gemma_model 
